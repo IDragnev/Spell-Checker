4cf8c7189fc3cede10873be06dfb03ad44088a1b
IDragnev <iddragnev@gmail.com>
2020-01-11 12:21:07 +0200

    Add tests for WordCounter

--- src/word_counter.rs
+++ src/word_counter.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::fmt;
 
 pub struct WordCounter {
     words_map: HashMap<String, u32>,
@@ -12,19 +13,16 @@ impl WordCounter {
     }
 
     pub fn from_str(input: &str) -> Self {
-        let mut counter = WordCounter {
-            words_map: HashMap::new(),
-        };     
+        let mut counter = Self::new();
         for word in input.lines()
             .map(|line| crate::clean_line(line))
-            .flat_map(|line| line.split_whitespace())
+            .flat_map(|line| to_words(line))
         {
-            let count = counter.add(word);
+            counter.add(&word);
         }
-
         counter
     }
- 
+
     pub fn add(&mut self, item: &str) {
         let word = item.trim().to_lowercase();
         let count = self.words_map.entry(word).or_insert(0);
@@ -32,12 +30,9 @@ impl WordCounter {
     }
 
     pub fn words(&self) -> Vec<&String> {
-        let vec = self.words_map
-            .iter()
-            .map(|(word, _)| word)
-            .collect::<Vec<&String>>();
-        vec.sort_unstable_by(|&a, &b| a.cmp(b));
-        vec
+        let mut words = self.words_map.keys().collect::<Vec<&String>>();
+        words.sort_unstable_by(|&a, &b| a.cmp(b));
+        words
     }
 
     pub fn get(&self, word: &str) -> u32 {
@@ -45,27 +40,65 @@ impl WordCounter {
     }
 
     pub fn total_count(&self) -> u32 {
-        self.words_map
-        .iter()
-        .fold(0, |sum, (_, count)| sum + count)
+        self.words_map.values().sum()
     }
 }
 
-/// Искаме да можем да напечатаме един `WordCounter` с цел дебъгване.
-///
-/// - Първи ред: `WordCounter, total count: {}`, форматирано с `total_count`.
-/// - Останалите редове: Всяка една дума, изчистена както е описано горе с `add`, с брой на
-/// срещането ѝ, примерно: "foo: 13"
-///
-/// Всеки ред се очаква да завършва с `\n`, включително последния. Думите трябва да са сортирани по
-/// брой на срещанията, най-честите трябва да са първи. Примерно:
-///
-///     WordCounter, total count: 25
-///     foo: 13
-///     bar: 12
-///
 impl std::fmt::Display for WordCounter {
     fn fmt(&self, f: &mut fmt::Formatter) -> std::fmt::Result {
-        unimplemented!()
+        let _ = writeln!(f, "WordCounter, total count: {}", self.total_count())?;
+        let mut pairs = self.words_map.iter().collect::<Vec<(&String, &u32)>>();
+        pairs.sort_unstable_by(|(_, x), (_, y)| y.cmp(&x));
+        for (word, count) in &pairs {
+            let _ = writeln!(f, "{}: {}", word, count)?;
+        }
+        Ok(())
+    }
+}
+
+fn to_words(line: String) -> Vec<String> {
+    line
+    .split_whitespace()
+    .map(|word| word.to_owned())
+    .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn default_counter_has_no_words() {
+        let counter = WordCounter::new();
+        assert!(counter.words().is_empty());
+        assert!(counter.total_count() == 0);
+        assert!(counter.get("random") == 0);
+    }
+
+    #[test]
+    fn counter_from_string() {
+        let text = "first line\nSecond LiNe\n THIRD LINE\n";
+        let expected_words = vec!["first", "line", "second", "third"];
+        
+        let counter = WordCounter::from_str(text);
+
+        assert_eq!(counter.words(), expected_words); 
+        assert_eq!(counter.total_count(), 6);
+        assert_eq!(counter.get("line"), 3);
+        assert_eq!(counter.get("first"), 1);
+        assert_eq!(counter.get("second"), 1);
+        assert_eq!(counter.get("third"), 1);
+        assert_eq!(counter.get("not-contained"), 0);
+    }   
+
+    #[test]
+    fn add() {
+        let mut counter = WordCounter::new();
+
+        for word in ["word", "Word", " word ", " WORD "].iter() {
+            counter.add(word);
+        }
+
+        assert_eq!(counter.get("word"), 4);
     }
 }
